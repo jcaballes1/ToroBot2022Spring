670ec5eaac617a8561124ab96349a2e1ccf3c62a
jcaballes1 <74940209+jcaballes1@users.noreply.github.com>
2022-04-05 00:20:18 -0700

    Delete ToroTestBotv2-src directory

--- ToroTestBotv2-src/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\torotestbotv2-Web-Deploy.pubxml -p:Password=iElJMKQNw55Tyn1BXzBK3zjal1e65XCWA88Eecfexuex0Yc0dNdJ3szKbbEp
-
