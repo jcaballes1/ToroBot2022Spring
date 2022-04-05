1135d253e462e3d83aea37c0820505d20504375f
jcaballes1 <74940209+jcaballes1@users.noreply.github.com>
2022-04-05 00:19:51 -0700

    Add files via upload

---
+++ ToroTestBotv2-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\torotestbotv2-Web-Deploy.pubxml -p:Password=iElJMKQNw55Tyn1BXzBK3zjal1e65XCWA88Eecfexuex0Yc0dNdJ3szKbbEp
+
